b1e86886cd2184a87823b83a692204be9d607dec
Saleh <saleh@grasppe.com>
2019-03-19 11:51:56 -0400

    Refactor parser work out of pholio (downstreams to Quench)

---
+++ packages/parser/lib/markup.js
@@ -0,0 +1,32 @@
+export * from '@smotaal/tokenizer/browser/markup.js';
+export * from './entities.js';
+// import {entities} from '@smotaal/tokenizer/extensions/common/patterns.js';
+
+// import {raw} from './helpers.js';
+
+// export const {
+// 	UnicodeIdentifier,
+// 	MarkdownIdentityPrefixer,
+// 	MarkdownIdentityJoiner,
+// 	MarkdownIdentityWord,
+// 	MarkdownIdentity,
+// } = (({
+// 	IdentifierStart,
+// 	IdentifierPart,
+// 	UnicodeIdentifierStart = IdentifierStart.slice(2),
+// 	UnicodeIdentifierPart = IdentifierPart.slice(2),
+// 	UnicodeIdentifier = raw`[${UnicodeIdentifierStart}][${UnicodeIdentifierPart}]*`,
+// 	MarkdownWordPrefixes = raw`$@`,
+// 	MarkdownWordPrefix = raw`[${MarkdownWordPrefixes}]?`,
+// 	MarkdownWord = raw`${MarkdownWordPrefix}${UnicodeIdentifier}`,
+// 	MarkdownWordJoiners = raw` \\\/:_\-\xA0\u2000-\u200B\u202F\u2060`,
+// 	MarkdownWordJoiner = raw`[${MarkdownWordJoiners}]+`,
+// 	MarkdownIdentity = raw`(?:\s|\n|^)(${MarkdownWord}(?:${MarkdownWordJoiner}${MarkdownWord})*(?=\b[\s\n]|$))`,
+// }) => ({
+// 	UnicodeIdentifier: new RegExp(UnicodeIdentifier, 'u'),
+// 	MarkdownIdentityPrefixer: new RegExp(raw`^[${MarkdownWordPrefixes}]?`, 'u'),
+// 	MarkdownIdentityJoiner: new RegExp(raw`[${MarkdownWordJoiners}]+`, 'ug'),
+// 	MarkdownIdentityWord: new RegExp(MarkdownWord, 'u'),
+// 	MarkdownIdentity: new RegExp(MarkdownIdentity, 'u'),
+// 	// MarkdownIdentitySeparators: new RegExp(raw`[${MarkdownWordPrefixes}${MarkdownWordJoiners}]+`, 'ug')
+// }))(entities.es);
